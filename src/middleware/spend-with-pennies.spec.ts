429c478ded07c4ad39a8e39862c46ccf7bd0f80f
Wilson Zhu <wilson.zhu@Wilsons-MacBook-Pro.local>
2019-08-02 18:39:34 -0500

    fixed glob pattern for running spec files

--- src/middleware/spend-with-pennies.spec.ts
+++ src/middleware/spend-with-pennies.spec.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 import { SpendWithPennies } from './spend-with-pennies';
 
+// TODO
 describe('SpendWithPennies middleware', () => {
   it('passes to the next middleware if the url does not match', () => {
 
